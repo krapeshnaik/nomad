da445ce1b501b19e57f71c37b523b496d0a7b2d7
Krapesh <krapeshnaik@gmail.com>
2018-03-07 23:30:46 +0530

    fix(App): Cleanup, Startup script
    Cleanup, Startup script

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const goNomad = require('./route_handlers/index.route');
+const goNomad = require('./services');
 
 const app = express();
 
 app.get('/', goNomad);
 
 app.listen(3000, () => {
-    console.log('nomad @home:3000');
+    console.log('go nomad @5555');
 });
